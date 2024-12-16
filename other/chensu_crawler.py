959aac96a8b756c7a234accf09cd8e860726214e
hujt <89085657@qq.com>
2024-12-16 16:56:52 +0800

    新增客户数据多个账号的数据读取

--- other/chensu_crawler.py
+++ other/chensu_crawler.py
@@ -551,7 +551,7 @@ def crawler_websites_data(websites_by_code: List[dict], websites_no_code: List[d
         driver = init_chrome(GOL.chromedriver_path, GOL.download_path, chrome_path=GOL.chrome_path, is_proxy=False)
         url2class: Dict[str, WebAbstract] = {
             WEBURL.spfj: SPFJWebCustom(driver, WEBURL.spfj),
-            WEBURL.inca: INCAWebCustom(driver, WEBURL.inca),
+            WEBURL.inca: INCAWebCustom(driver, GOL.download_path, WEBURL.inca),
             WEBURL.ly: LYWebCustom(driver, WEBURL.ly)
         }
         is_error = crawler_general(websites_no_code, url2class)
